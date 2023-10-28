63c49b3a8043f76157c9e84d7f3c3702e8f79fed
Juan Diaz <juanchodis@hotmail.com>
2023-10-28 17:05:39 -0400

    global.css added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import "./globals.css";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      App
+      <h1 className="h1-bold">Juan Diaz</h1>
+      <p className="h1-semibold">Juan Diaz</p>
+    </div>
+  );
 };
 
 export default App;
