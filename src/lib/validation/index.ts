1221f270b20c2aea4feb6309112ec8756e0b197f
Juan Diaz <juanchodis@hotmail.com>
2023-10-30 14:36:11 -0400

    AUTO PAGES | Working on the signUp  page - The Form section

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,17 @@
+import * as z from "zod";
+
+export const SignupValidation = z.object({
+  name: z
+    .string()
+    .min(2, { message: "Name is too short" })
+    .max(50, { message: "Name is too long" }),
+  username: z
+    .string()
+    .min(2, { message: "Username is too short" })
+    .max(50, { message: "Username is too short" }),
+  email: z.string().email({ message: "Please enter a valid email" }).e,
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters" })
+    .max(60, { message: "Password is too long" }),
+});
