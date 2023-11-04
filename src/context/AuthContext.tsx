3328fc55728ccb902eb8253e55c903e5342b5360
Juan Diaz <juanchodis@hotmail.com>
2023-11-04 13:08:12 -0400

    [fix] syntax error

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -56,8 +56,11 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   };
 
   useEffect(() => {
-    if (localStorage.getItem("cookieFallback") === "[]") navigate("/sign-in");
-    localStorage.getItem("cookieFallback") === null;
+    if (
+      localStorage.getItem("cookieFallback") === "[]" ||
+      localStorage.getItem("cookieFallback") === null
+    )
+      navigate("/sign-in");
 
     checkAuthUser();
   }, []);
