3190e9d1c4fb04fa78fe55b792279806cb321a75
Juan Diaz <juanchodis@hotmail.com>
2023-11-06 16:29:00 -0500

    [working on] homepage

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -7,7 +7,11 @@ import {
   useInfiniteQuery,
 } from "@tanstack/react-query";
 
-import { createUserAccount, signInAccount } from "@/lib/appwrite/api";
+import {
+  createUserAccount,
+  signInAccount,
+  signOutAccount,
+} from "@/lib/appwrite/api";
 import { INewUser } from "@/types";
 
 // ============================================================
@@ -24,5 +28,11 @@ export const useSignInAccount = () => {
   return useMutation({
     mutationFn: (user: { email: string; password: string }) =>
       signInAccount(user),
+  });
+};
+
+export const useSignOutAccount = () => {
+  return useMutation({
+    mutationFn: signOutAccount,
   });
 };
