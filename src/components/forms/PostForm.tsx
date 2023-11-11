7bd0109ec5f0f994993cb5bea8ac812413ca6e31
Juan Diaz <juanchodis@hotmail.com>
2023-11-11 13:00:06 -0500

    [DONE] Post CRUD - Edit post

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -16,20 +16,24 @@ import { Textarea } from "../ui/textarea";
 import FileUploader from "../shared/FileUploader";
 import { PostValidation } from "@/lib/validation";
 import { Models } from "appwrite";
-import { useCreatePost } from "@/lib/react-query/queriesAndMutations";
+import {
+  useCreatePost,
+  useUpdatePost,
+} from "@/lib/react-query/queriesAndMutations";
 import { useUserContext } from "@/context/AuthContext";
 import { useToast } from "../ui/use-toast";
 import { useNavigate } from "react-router-dom";
 
 type PostFormProps = {
   post?: Models.Document;
+  action: "Create" | "Update";
 };
 
-const PostForm = ({ post }: PostFormProps) => {
-  const {
-    mutateAsync: createPost,
-    // isPending: isLoadingCreate
-  } = useCreatePost();
+const PostForm = ({ post, action }: PostFormProps) => {
+  const { mutateAsync: createPost, isPending: isLoadingCreate } =
+    useCreatePost();
+  const { mutateAsync: updatePost, isPending: isLoadingUpdate } =
+    useUpdatePost();
   // Hooks:
   const { user } = useUserContext();
   const { toast } = useToast();
@@ -48,6 +52,25 @@ const PostForm = ({ post }: PostFormProps) => {
 
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof PostValidation>) {
+    // if action is update, update post
+    if (action === "Update") {
+      const updatedPost = await updatePost({
+        ...values,
+        postId: post?.$id,
+        imageId: post?.imageId,
+        imageUrl: post?.imageUrl,
+      });
+
+      // if NO updated post, show error
+      if (!updatedPost) {
+        toast({
+          title: "Please try again",
+        });
+      }
+      // if updated post, redirect to post page
+      return navigate("/posts/${post?.$id}");
+    }
+    // if it doesn't edit the post, create post
     const newPost = await createPost({
       ...values,
       userId: user.id,
@@ -62,7 +85,7 @@ const PostForm = ({ post }: PostFormProps) => {
     // if new post, redirect to home page
     navigate("/");
   }
-
+  console.log(post?.imageUrl);
   return (
     <Form {...form}>
       <form
@@ -142,8 +165,10 @@ const PostForm = ({ post }: PostFormProps) => {
           <Button
             type="submit"
             className="shad-button_primary whitespace-nowrap"
+            disabled={isLoadingCreate || isLoadingUpdate}
           >
-            Submit
+            {isLoadingCreate || (isLoadingUpdate && "Loading...")}
+            {action} Post
           </Button>
         </div>
       </form>
